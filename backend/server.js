6de5e085d06c062ad3ce79ddf3543e2272155248
Bui Hoang Long <nhulac2004@gmail.com>
2026-09-17 22:48:27 +0700

    Update news

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const crypto = require('crypto');
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 
 // ====================================================
